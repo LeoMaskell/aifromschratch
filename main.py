04e78846f309818a9c464eb46d900081a1f13b40
Leo Maskell <22maskelll@bluecoatstudent.org.uk>
2026-04-20 17:01:28 +0100

    main.py
    - done backprop, it is 25% accurattee????

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import numpy as np
 # XOR NN from scratch
 # 2:relu:4:sigmoid:1
 
+lr = 0.1
 
+
+# sigmoid function
 def sigmoid(val):
     value = (np.exp(-val) + 1) ** -1
     return value  # sigmoid(0) = 0.5
@@ -20,22 +23,35 @@ b2 = np.random.randn(1, 1) * 0.1
 
 # print(w1, b1, w2, b2)
 
-# first forward pass
-z1 = np.dot(x, w1) + b1
-a1 = np.maximum(0, z1)
-z2 = np.dot(a1, w2) + b2
-a2 = sigmoid(z2)  # the prediction from the net
+for i in range(10000):
+    # forward pass
+    z1 = np.dot(x, w1) + b1
+    a1 = np.maximum(0, z1)
+    z2 = np.dot(a1, w2) + b2
+    a2 = sigmoid(z2)  # the prediction from the net
 
-print(f"random preds: {a2}")
+    print(f"random preds: {a2}")
 
-loss = np.mean((a2 - y) ** 2)  # mse
-print(f"random loss: {loss}")
+    # backprop
+    # derivative of mse = 2(a2 - y) over n -->(4)
 
-# backprop
-# derivative of mse = 2(a2 - y) over n -->(4)
+    dL_da2 = (a2 - y) / 2
+    da2_dz2 = a2 * (1 - a2)
+    dL_dz2 = dL_da2 * da2_dz2
+    dL_dw2 = np.dot(a1.T, dL_dz2)
+    dL_db2 = np.sum(dL_dz2, axis=0, keepdims=True)
+    dL_da1 = np.dot(dL_dz2, w2.T)
+    dL_dz1 = dL_da1 * (z1 > 0)
+    dL_dw1 = np.dot(x.T, dL_dz1)
+    dL_db1 = np.sum(dL_dz1, axis=0, keepdims=True)
 
-dL_da2 = (a2 - y) / 2
-da2_dz2 = a2 * (1 - a2)
-dL_dz2 = dL_da2 * da2_dz2
-dL_dw2 = np.dot(a1.T, dL_dz2)
-dL_db2 = np.sum(dL_dz2, axis=0, keepdims=True)
+    w1 = w1 - lr * dL_dw1
+    w2 = w2 - lr * dL_dw2
+    b1 = b1 - lr * dL_db1
+    b2 = b2 - lr * dL_db2
+
+    loss = np.mean((a2 - y) ** 2)  # mse
+    print(f"loss: {loss}")
+
+    if i % 1000 == 0:
+        print(f"loss: {loss}")
